811e79134755501b0d4e409b14e3c373ed6a4fdc
unknown <b.mahendrasunny@gmail.com>
2023-01-06 19:23:33 +0530

    Added Overloading Type InstanceType

--- CoreJavConcepts/src/com/corejava/basics/OverloadingTypeInstanceType.java
+++ CoreJavConcepts/src/com/corejava/basics/OverloadingTypeInstanceType.java
@@ -2,9 +2,13 @@ package com.corejava.basics;
 
 public class OverloadingTypeInstanceType {
     public static void main(String[] args) {
-        OverloadingTypeInstanceType overloadingTypeInstanceType = new OverloadingTypeInstanceType();
+        /*OverloadingTypeInstanceType overloadingTypeInstanceType = new OverloadingTypeInstanceType();
         //overloadingTypeInstanceType.test("test1", "test2");
-        overloadingTypeInstanceType.test(null, new Person());
+        overloadingTypeInstanceType.test(null, new Person());*/
+
+        Person person = new Person();
+        Person person2 = new Person("Bharath", 7000);
+        Person person3 = new Person(3, "Girish", 1000000);
     }
 
     /*void test(String str1, String str2){
@@ -21,5 +25,29 @@ public class OverloadingTypeInstanceType {
 }
 
 class Person {
+    int id;
+    String name;
+    double salary;
 
+    Person() {
+        System.out.println("Zero argument");
+    }
+
+    Person(String name, double salary) {
+        System.out.println("2 argument");
+        this.name = name;
+        this.salary = salary;
+    }
+
+    Person(int id, String name, double salary) {
+        System.out.println("3 argument");
+        this.salary = salary;
+        this.name = name;
+        this.id = id;
+    }
 }
+
+//Constructor:
+// it has the same name as class name
+//It is used for initializing the object
+//It does not have any return type
