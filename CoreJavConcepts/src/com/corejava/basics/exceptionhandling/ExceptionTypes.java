becc1712fb979c6f5999c33dcdef0e23cfdf2264
Bharath Kumar <bharathkumar12342@gmail.com>
2023-01-19 12:45:49 +0530

    Exception Types

---
+++ CoreJavConcepts/src/com/corejava/basics/exceptionhandling/ExceptionTypes.java
@@ -0,0 +1,24 @@
+package com.corejava.basics.exceptionhandling;
+
+import java.util.Scanner;
+// Unchecked Exception
+public class ExceptionTypes {
+
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the value of A");
+        int a = scanner.nextInt();
+        System.out.println("Enter the value of B");
+        int b = scanner.nextInt();
+        try {
+            int c = a / b;
+            System.out.println("Result " + c);
+        } catch (ArithmeticException ex){
+            System.out.println("There is an Error Please Check");
+        }
+        System.out.println("Some important code");
+
+}
+}
+
